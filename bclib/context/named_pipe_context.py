4284b35bf2081dd5f08cbb23561bb43df2a59ec8
Qamsari <ali.qamsari@gmail.com>
2022-06-15 15:23:17 +0430

    [bug] NamedPipeContext: change message property data type from dict to DictEx #9

--- bclib/context/named_pipe_context.py
+++ bclib/context/named_pipe_context.py
@@ -8,7 +8,7 @@ from bclib.utility import DictEx
 class NamedPipeContext(Context):
     """Context for named pipe request"""
 
-    def __init__(self, named_pipe_message: DictEx, raw_message: str, dispatcher: 'dispatcher.IDispatcher'):
+    def __init__(self, named_pipe_message: 'dict', raw_message: str, dispatcher: 'dispatcher.IDispatcher'):
         super().__init__(dispatcher)
-        self.message: DictEx = named_pipe_message
+        self.message: DictEx = DictEx(named_pipe_message)
         self.raw_message = raw_message
