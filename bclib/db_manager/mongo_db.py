e6b1f2ed88d41925f14f1de19fc7bd2b36a7a4be
mahshidrz <mahshidrezavandi.64@gmail.com>
2022-07-31 13:17:38 +0430

    Update mongo_db.py
    handle singleton connection

--- bclib/db_manager/mongo_db.py
+++ bclib/db_manager/mongo_db.py
@@ -3,12 +3,22 @@ from ..db_manager.db import Db
 
 class MongoDb(Db):
     """Mongo implementation of Db wrapper"""
+    client = None
+
+    def get_instance(self):
+        if self.client is None:
+            MongoDb(self.connection_string)
+        return self.client
 
     def __init__(self, connection_string: str) -> None:
         super().__init__()
         import pymongo
-        self.client = pymongo.MongoClient(connection_string)
+        self.connection_string = connection_string
+        if self.client is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            self.client = pymongo.MongoClient(self.connection_string)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.client.close()
-        return super().__exit__(exc_type, exc_val, exc_tb)
+    # def __exit__(self, exc_type, exc_val, exc_tb):
+    #     self.client.close()
+    #     return super().__exit__(exc_type, exc_val, exc_tb)
