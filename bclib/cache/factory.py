7c34b0256c0295643a08e322baf7d3386ef97e08
amir <kmg.d126@gmail.com>
2022-08-16 17:27:59 +0430

    add initial_memory_cache

--- bclib/cache/factory.py
+++ bclib/cache/factory.py
@@ -1,6 +1,7 @@
 from bclib.utility import DictEx
 from ..cache.cache_manager import CacheManager
 from ..cache.in_memory_cache_manager import InMemoryCacheManager
+from ..cache.initial_memory_cache_manager import InitialMemoryCacheManager
 from ..cache.no_cache import NoCacheManager
 
 
@@ -9,6 +10,8 @@ def create_caching(options: DictEx) -> CacheManager:
     cache_type = options.type if options is not None and "type" in options else "none"
     if cache_type == "memory":
         ret_val = InMemoryCacheManager(options)
+    elif cache_type == "initial_memory":
+        ret_val = InitialMemoryCacheManager(options)
     elif cache_type == "none":
         ret_val = NoCacheManager()
     else:
