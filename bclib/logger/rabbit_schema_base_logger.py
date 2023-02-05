85f59e6b541dece3ae6b05d83c9625c835647866
abolfazlj00 <jelodarabolfazl@gmail.com>
2023-02-05 17:43:05 +0330

    #35

--- bclib/logger/rabbit_schema_base_logger.py
+++ bclib/logger/rabbit_schema_base_logger.py
@@ -14,13 +14,19 @@ class RabbitSchemaBaseLogger(SchemaBaseLogger):
     async def _save_schema_async(self, schema: dict):
         def send_to_rabbit(options):
             import pika
+            connection_options = options.connection
+            queue = connection_options.queue
+            durable = connection_options.durable if connection_options.has("durable") else False
+            passive=connection_options.passive if connection_options.has("passive") else False
+            exclusive=connection_options.exclusive if connection_options.has("exclusive") else False
+            auto_delete=connection_options.auto_delete if connection_options.has("auto_delete") else False
             connection = pika.BlockingConnection(
-                pika.URLParameters(options.connection.url))
+                pika.URLParameters(connection_options.url))
             channel = connection.channel()
-            channel.queue_declare(queue=options.connection.queue)
+            channel.queue_declare(queue=queue, durable=durable, passive=passive, exclusive=exclusive, auto_delete=auto_delete)
             channel = connection.channel()
             channel.basic_publish(
-                exchange='', routing_key=options.connection.queue, body=json.dumps(schema))
+                exchange='', routing_key=queue, body=json.dumps(schema))
         loop = asyncio.get_running_loop()
         future = loop.run_in_executor(None, send_to_rabbit, self.options)
         await future
