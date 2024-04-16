853861f5743970777e2aa047a45cc627c857c72d
abolfazlj00 <jelodarabolfazl@gmail.com>
2024-04-16 16:41:12 +0330

    #63

--- bclib/exception/__init__.py
+++ bclib/exception/__init__.py
@@ -3,3 +3,5 @@ from bclib.exception.unauthorized_err import UnauthorizedErr
 from bclib.exception.internal_server_err import InternalServerErr
 from bclib.exception.not_found_err import NotFoundErr
 from bclib.exception.handler_not_found_err import HandlerNotFoundErr
+from bclib.exception.bad_request_err import BadRequestErr
+from bclib.exception.forbidden_err import ForbiddenErr
---
+++ bclib/exception/forbidden_err.py
@@ -0,0 +1,7 @@
+from bclib.utility.http_status_codes import HttpStatusCodes
+from .short_circuit_err import ShortCircuitErr
+
+
+class ForbiddenErr(ShortCircuitErr):
+    def __init__(self, message: str = None, data: 'dict' = None):
+        super().__init__(HttpStatusCodes.FORBIDDEN, 'http-403', message, data)
